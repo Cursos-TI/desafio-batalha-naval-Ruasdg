7beb0a6a6ed660d8064719408d033746bb81dcd5
Ruasdg <ruasdg6@gmail.com>
2025-05-24 13:36:34 +0000

    fiz um jogo de batalha batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,125 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAM 5
+#define AGUA '~'
+#define BARCO 'B'
+#define TIRO_ACERTO 'X'
+#define TIRO_AGUA 'O'
+
+void inicializarTabuleiro(char tabuleiro[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            tabuleiro[i][j] = AGUA;
+}
+
+void imprimirTabuleiro(char tabuleiro[TAM][TAM], int ocultar) {
+    printf("  ");
+    for (int i = 0; i < TAM; i++)
+        printf("%d ", i);
+    printf("\n");
+
+    for (int i = 0; i < TAM; i++) {
+        printf("%d ", i);
+        for (int j = 0; j < TAM; j++) {
+            if (ocultar && tabuleiro[i][j] == BARCO)
+                printf("%c ", AGUA);
+            else
+                printf("%c ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void posicionarBarcos(char tabuleiro[TAM][TAM], int qtdBarcos) {
+    int x, y;
+    for (int i = 0; i < qtdBarcos; i++) {
+        do {
+            printf("Digite a posição do barco #%d (linha coluna): ", i + 1);
+            scanf("%d %d", &x, &y);
+        } while (x < 0 || x >= TAM || y < 0 || y >= TAM || tabuleiro[x][y] == BARCO);
+        tabuleiro[x][y] = BARCO;
+    }
+}
+
+int atirar(char tabuleiro[TAM][TAM], int x, int y) {
+    if (tabuleiro[x][y] == BARCO) {
+        tabuleiro[x][y] = TIRO_ACERTO;
+        return 1;
+    } else if (tabuleiro[x][y] == AGUA) {
+        tabuleiro[x][y] = TIRO_AGUA;
+        return 0;
+    }
+    return -1; // Já atirado
+}
+
+int barcosRestantes(char tabuleiro[TAM][TAM]) {
+    int contador = 0;
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            if (tabuleiro[i][j] == BARCO)
+                contador++;
+    return contador;
+}
+
+void limparTela() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char tabuleiro1[TAM][TAM], tabuleiro2[TAM][TAM];
+    int x, y, jogadorAtual = 1, resultado, barcos = 3;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    inicializarTabuleiro(tabuleiro1);
+    inicializarTabuleiro(tabuleiro2);
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    printf("Jogador 1, posicione seus barcos:\n");
+    posicionarBarcos(tabuleiro1, barcos);
+    limparTela();
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    printf("Jogador 2, posicione seus barcos:\n");
+    posicionarBarcos(tabuleiro2, barcos);
+    limparTela();
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    while (1) {
+        printf("Vez do Jogador %d\n", jogadorAtual);
+        if (jogadorAtual == 1)
+            imprimirTabuleiro(tabuleiro2, 1);
+        else
+            imprimirTabuleiro(tabuleiro1, 1);
+
+        printf("Digite coordenadas do tiro (linha coluna): ");
+        scanf("%d %d", &x, &y);
+
+        if (x < 0 || x >= TAM || y < 0 || y >= TAM) {
+            printf("Coordenadas inválidas. Tente novamente.\n");
+            continue;
+        }
+
+        if (jogadorAtual == 1)
+            resultado = atirar(tabuleiro2, x, y);
+        else
+            resultado = atirar(tabuleiro1, x, y);
+
+        if (resultado == 1)
+            printf("Acertou!\n");
+        else if (resultado == 0)
+            printf("Água!\n");
+        else
+            printf("Você já atirou aí!\n");
+
+        if (barcosRestantes(jogadorAtual == 1 ? tabuleiro2 : tabuleiro1) == 0) {
+            printf("Jogador %d venceu!\n", jogadorAtual);
+            break;
+        }
+
+        jogadorAtual = (jogadorAtual == 1) ? 2 : 1;
+    }
 
     return 0;
 }
